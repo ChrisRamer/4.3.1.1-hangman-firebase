2833ad2a350a75c5df9c6f0d95dd334355757b23
Chris Ramer <chrisramer.97@gmail.com>
2021-12-21 13:59:33 -0800

    uhh lots of stuff and letter guessing & sentence displaying work

--- src/components/GameControl.js
+++ src/components/GameControl.js
@@ -7,16 +7,53 @@ class AppControl extends React.Component {
 		super(props);
 		this.state = {
 			inGameSetup: true,
-			currentGame: null
+			currentGame: null,
+			gameState: "NOT_STARTED",
+			selectedSentence: null,
+			lettersNotGuessed: [
+				"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+			],
+			currentImage: 0
 		};
 	}
 
+	getSentence = (wordCount) => {
+		const sentences = [
+			"Hello beautiful world!", // 3
+			"Words are very fun!", // 4
+			"Words make up a sentence.", // 5
+			"This is a truly incredible sentence!", // 6
+			"This sentence definitely has exactly 7 words.", // 7
+			"This sentence wholeheartedly has more than 5 words.", // 8
+			"This is a sentence that is a bit longer...", // 9
+			"Did you know, a sentence is comprised of several words?" // 10
+		]
+
+		const selectedSentence = sentences.find(x => x.split(" ").length === wordCount);
+		return selectedSentence;
+	}
+
 	handleStartingNewGame = (newGame) => {
-		console.log("New game started with "  + newGame.wordCount + " words!");
 		this.setState({
 			currentGame: newGame,
+			selectedSentence: this.getSentence(newGame.wordCount),
 			inGameSetup: false
 		});
+	}
+
+	handleGuessingLetter = (letterGuessed) => {
+		console.log("Guessed letter " + letterGuessed + "!");
+		const newLettersNotGussed = this.state.lettersNotGuessed.filter(x => x !== letterGuessed);
+
+		this.setState({
+			lettersNotGuessed: newLettersNotGussed
+		});
+
+		if (this.state.selectedSentence.toUpperCase().includes(letterGuessed)) {
+			console.log("Guessed correctly!");
+		} else {
+			console.log("Guessed incorrectly!");
+		}
 	}
 
 	render() {
@@ -24,9 +61,8 @@ class AppControl extends React.Component {
 
 		if (this.state.inGameSetup) {
 			currentlyVisibleState = <GameSetup onNewGameCreation={this.handleStartingNewGame}  />;
-		}
-		else {
-			currentlyVisibleState = <GamePlay wordCount={this.state.currentGame.wordCount} />;
+		} else {
+			currentlyVisibleState = <GamePlay sentence={this.state.selectedSentence} lettersNotGuessed={this.state.lettersNotGuessed} onGuessedLetter={this.handleGuessingLetter} />;
 		}
 
 		return (
--- src/components/GamePlay.js
+++ src/components/GamePlay.js
@@ -2,13 +2,37 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function GamePlay(props) {
+
+	function  formatDisplayedSentence() {
+		let sentence = props.sentence.toLowerCase().replaceAll(' ', "\xa0\xa0");
+
+		props.lettersNotGuessed.map((letter) =>
+			sentence = sentence.replaceAll(letter.toLowerCase(), " _ ")
+		)
+
+		return sentence[0].toUpperCase() + sentence.slice(1);
+	}
+
 	return (
-		<p>Playing game with {props.wordCount} words!</p>
+		<React.Fragment>
+			<p>{formatDisplayedSentence()}</p>
+
+			<div className="letterButtons">
+				{props.lettersNotGuessed.map((letter, index) =>
+					<button key={index} type="submit" onClick={() => props.onGuessedLetter(letter)}>{letter}</button>
+				)}
+			</div>
+
+			<p>Playing game with sentence: {props.sentence}</p>
+		</React.Fragment>
 	)
+
 }
 
 GamePlay.propTypes = {
-	wordCount: PropTypes.number
+	sentence: PropTypes.string,
+	lettersNotGuessed: PropTypes.array,
+	onGuessedLetter : PropTypes.func
 }
 
 export default GamePlay;

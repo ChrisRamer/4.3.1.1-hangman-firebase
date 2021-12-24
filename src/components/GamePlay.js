5bd788f0e500acfa2fe790bc99aed6984032b39b
Chris Ramer <chrisramer.97@gmail.com>
2021-12-24 13:08:03 -0800

    Add image component

--- src/components/GamePlay.js
+++ src/components/GamePlay.js
@@ -1,35 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-import images from "./../img/index.js";
 import GameResult from "./GameResult";
+import Image from "./Image";
 
 function GamePlay(props) {
-
-	function getImage() {
-		switch (props.misses) {
-			case 0:
-				return images.image0;
-			case 1:
-				return images.image1;
-			case 2:
-				return images.image2;
-			case 3:
-				return images.image3;
-			case 4:
-				return images.image4;
-			case 5:
-				return images.image5;
-			default:
-				return images.image6;
-		}
-	}
 
 	return (
 		<React.Fragment>
 			<div class="gameplay">
-				<div class="image">
-					<img src={getImage()} alt={`You have made ${props.misses} incorrect guesses`} />
-				</div>
+				<Image misses={props.misses} />
 				<GameResult sentence={props.sentence} lettersNotGuessed={props.lettersNotGuessed} onGuessedLetter={props.onGuessedLetter} misses={props.misses} />
 			</div>
 		</React.Fragment>
